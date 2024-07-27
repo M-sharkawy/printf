a92d69146b3e2341c04434dea282e4eded6340d8
M-sharkawy <mostafasharks021@gmail.com>
2024-07-27 17:58:55 +0300

    print _ percent edits

---
+++ test sharkawy/_printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _printf - printing function to stdout
+ * @format: constant string
+ *
+ * Return: (length) which is the size of format
+ * otherwise - (-1)
+ */
+
+int _printf(const char *format, ...)
+{
+va_list ptr;
+	int length, i, j;
+	func_type types[] = {
+		{.t = 's', .f = print_str},
+		{.t = 'c', .f = print_char},
+		{.t = '%', .f = print_percent},
+		{'\0', NULL}
+	};
+
+	va_start(ptr, format);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+
+	i = 0;
+	for (; format[i]; i++)
+	{
+		if (format[i] != '%')
+		{
+			length += _putchar(format[i]);
+			continue;
+		}
+		j = 0;
+		while (types[j].t != '\0')
+		{
+			if (format[i + 1] == types[j].t)
+			{
+				length += types[j].f(ptr);
+				i++;
+				continue;
+			}
+			j++;
+		}
+	}
+
+	return (length);
+}
