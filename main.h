8365fd0826951b44311fab0f3e8b8d8fab5f4410
Martin George <martingeorge191110@gmail.com>
2024-07-27 16:21:57 +0300

    Create main.h File

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct funcType - to determine specifier and it's function
+ * @t: specifier type
+ * @f: pointer to function
+ */
+
+typedef struct funcType
+{
+	char t;
+	int (*f)(va_list);
+} func_type;
+
+
+#endif
