bd18c1c6a8604a6d7f72e13b530607c63bd49988
M-sharkawy <mostafasharks021@gmail.com>
2024-07-30 15:16:48 +0300

    buffer usage

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+#define Buffer_size 1024
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -7,7 +7,22 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buffer[Buffer_size];
+	static int i;
+
+	if (i >= Buffer_size || c == -1)
+	{
+		write(1, &buffer, i);
+		i = 0;
+	}
+
+	if (c != -1)
+	{
+		buffer[i++] = c;
+	}
+
+	return (1);
 }
